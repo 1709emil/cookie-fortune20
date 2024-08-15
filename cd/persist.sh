06659223c440b15dad6646ca91c9b2f46bf1a8e8
1709emil <1709emil@gmail.com>
2024-08-15 15:19:45 +0200

    deploy now clones repo first

--- cd/persist.sh
+++ cd/persist.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 KUBECONFIG_CONTENT="${KUBECONFIG}"
 
-if [ -z "$KUBECONFIG_CONTENT" ]; then
-  echo "KUBECONFIG environment variable is not set. Exiting..."
-  exit 1
-fi
-
 mkdir -p $HOME/.kube
-
 echo "$KUBECONFIG_CONTENT" > $HOME/.kube/kubeconfig
 
 chmod 600 $HOME/.kube/kubeconfig
