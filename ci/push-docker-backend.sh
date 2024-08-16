10d50f9b03749a8dd1e21aa70eb4d003e6c1ab8e
Emil Jørgensen <170903emil@gmail.com>
2024-08-16 08:59:19 +0200

    Added script for persisting to disk and job to deploy (#21)
    * 03 container orchestration (#18)
    
    * 03 - containers orchestration
    
    * 03 - containers orchestration updated to use separate images some action package
    
    ---------
    
    Co-authored-by: Emil Jørgensen <170903emil@gmail.com>
    
    * Rename workflow 03.yml to main-workflow.yml
    
    * switch from using github actor to just use hardcode name (#20)
    
    * added script for persisting the kubeconfig to disk
    
    * added delpoy job
    
    * rename persist file and add run command for deploy job
    
    * deploy now clones repo first
    
    * change path for running the script
    
    * test to see if it applys the deployment files and services
    
    * test to see dirs
    
    * remove scripts and echo cmd to step
    
    * fixed pathing in kubectl cmd
    
    * Changed apply command
    
    * test
    
    * trying to find path again.yml
    
    * Removed testing cmd.yml
    
    ---------
    
    Co-authored-by: hemgaard <59915695+Petervesthem@users.noreply.github.com>

--- ci/push-docker-backend.sh
+++ ci/push-docker-backend.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "$docker_password" | docker login ghcr.io --username "$docker_username" --password-stdin
-docker push "ghcr.io/$docker_username/cookie-fortune20-backend:1.0-${GIT_COMMIT::8}" 
-docker push "ghcr.io/$docker_username/cookie-fortune20-backend:latest" &
+docker push "ghcr.io/1709emil/cookie-fortune20-backend:1.0-${GIT_COMMIT::8}" 
+docker push "ghcr.io/1709emil/cookie-fortune20-backend:latest" &
 wait
